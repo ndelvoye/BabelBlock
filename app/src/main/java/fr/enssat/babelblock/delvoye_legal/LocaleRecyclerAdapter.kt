0d7a29d35ccbf2031f0978b13be952ab247e3d71
ndelvoye <nicolhas.delvoye@gmail.com>
2020-11-19 13:22:28 +0100

    Added Spanish, modified LocaleListItem UI

--- app/src/main/java/fr/enssat/babelblock/delvoye_legal/LocaleRecyclerAdapter.kt
+++ app/src/main/java/fr/enssat/babelblock/delvoye_legal/LocaleRecyclerAdapter.kt
@@ -1,13 +1,18 @@
 package fr.enssat.babelblock.delvoye_legal
 
+import android.content.res.Resources
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import fr.enssat.babelblock.delvoye_legal.models.LocaleItem
+import java.util.*
+import kotlin.collections.ArrayList
 
 class LocaleRecyclerAdapter : RecyclerView.Adapter<LocaleRecyclerAdapter.ViewHolder>() {
     private var localeItemList =  ArrayList<LocaleItem>()
@@ -47,13 +52,26 @@ class LocaleRecyclerAdapter : RecyclerView.Adapter<LocaleRecyclerAdapter.ViewHol
     }
 
     class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val text: TextView = itemView.findViewById(R.id.text_locale)
+        private val languageFlagImg: ImageView = itemView.findViewById(R.id.language_flag_img)
+        val chainTranslationText: TextView = itemView.findViewById(R.id.chain_translation_text)
         val editButton: Button = itemView.findViewById(R.id.edit_locale_button)
         val deleteButton: Button = itemView.findViewById(R.id.delete_locale_button)
 
         fun bind(item: LocaleItem) {
-            val res = itemView.context.resources
-            text.text = item.language.displayLanguage
+            languageFlagImg.setImageResource(stringToFlagImg(item.language.language))
+        }
+
+        private fun stringToFlagImg(s: String): Int {
+            return when (s) {
+                Locale.FRENCH.language -> R.mipmap.france_flag
+                Locale.ENGLISH.language -> R.mipmap.united_kingdom_flag
+                "es" -> R.mipmap.spain_flag
+                Locale.CHINESE.language -> R.mipmap.china_flag
+                Locale.ITALIAN.language -> R.mipmap.italy_flag
+                Locale.JAPANESE.language -> R.mipmap.japan_flag
+                Locale.GERMAN.language -> R.mipmap.germany_flag
+                else -> throw Exception("Locale not recognized : $s")
+            }
         }
 
         companion object {

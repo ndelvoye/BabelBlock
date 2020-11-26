20ba5854c6d3a9222fda72097e7593a11fd33a50
galeadon <alegalbtssio@gmail.com>
2020-11-26 10:11:46 +0100

    add button text to speech

---
+++ app/build/generated/source/buildConfig/debug/fr/enssat/babelblock/delvoye_legal/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fr.enssat.babelblock.delvoye_legal;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "fr.enssat.babelblock.delvoye_legal";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
